07864d8c41497f7e02111093e152f4c36e2aeadc
Andrew Vasilyev <a.vasilyev@corvusalba.ru>
2016-04-15 15:40:43 +0300

    some refactoring

--- src/TelegramSouls.Server.CLI/Program.cs
+++ src/TelegramSouls.Server.CLI/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
 using TelegramSouls.Server.Telegram;
 
 namespace TelegramSouls.Server.CLI
@@ -8,11 +6,6 @@ namespace TelegramSouls.Server.CLI
     class Program
     {
         static void Main()
-        {
-            RunAsync().Wait();
-        }
-
-        static async Task RunAsync()
         {
             var c = new Client("188718743:AAEi9xE4Y8l-0q1KlqtsOYUEKViW6pw0y2A");
             var queue = new MessageQueue();
